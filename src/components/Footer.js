2831c76c19d5b277e80b9a5e5785724a171d7f18
Mehdih77 <mehdiwork77@gmail.com>
2021-09-09 13:50:02 +0430

    Complete Functionality

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,49 @@
+import { useState } from "react"
+import { useTodoContext } from "../context/TodoProvider";
+
 export default function Footer() {
+
+    const [completeAll, setCompleteAll] = useState(false);
+    const [todos,setTodos] = useTodoContext();
+
+    // complete All todos with One click
+    const handleCompleteAll = () => {
+        setCompleteAll(!completeAll);
+        const newTodos = [...todos];
+        newTodos.forEach(todo => {
+            todo.complete = !completeAll
+        })
+        setTodos(newTodos);
+    }
+
+    const handleDeleteTodo = () => {
+        const newTodos = todos.filter(todo => {
+            return todo.complete === false
+        })
+        setTodos(newTodos);
+        setCompleteAll(false);
+    }
+
+    const remainTodo = () => {
+        return todos.filter(todo => todo.complete === false)
+    }
+
     return (
         <footer className="row">
-            <label htmlFor="">
-                <input type="checkbox" name="all" id="all" />
-                    All
-            </label>
-            <p>You have 0 to do</p>
-            <button id="delete">Delete</button>
+            {todos.length === 0 ? 
+                <h2>Nothing To Do</h2>
+                 : 
+                <>
+                <label htmlFor="all">
+                    <input type="checkbox" name="all" id="all"
+                    onChange={handleCompleteAll}
+                    checked={completeAll} />
+                        All
+                </label>
+                <p>You have {remainTodo().length} to do</p>
+                <button onClick={handleDeleteTodo} id="delete">Delete</button>
+                </>
+            }
         </footer>
     )
 }
--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,22 +1,29 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useTodoContext } from "../context/TodoProvider";
 
 export default function FormInput() {
 
     const [name, setName] = useState('');
     const [todos,setTodos] = useTodoContext();
+    const inputRef = useRef();
+    useEffect(() => {
+        inputRef.current.focus();
+    }, [])
 
     const handleSubmitTodo = (e) => {
         e.preventDefault();
         setTodos([...todos, {name, complete: false, id: Math.random().toFixed(2)}]);
         setName('');
+        inputRef.current.focus();
     }
 
     return (
         <form autoComplete='off' onSubmit={handleSubmitTodo}>
             <input type="text" name='todos' id='todos'
             required placeholder='What need to be done..'
-            value={name} onChange={(e) => setName(e.target.value.toLowerCase())} />
+            ref={inputRef}
+            value={name}
+            onChange={(e) => setName(e.target.value.toLowerCase())} />
             <button type='submit'>Create</button>
         </form>
     )
